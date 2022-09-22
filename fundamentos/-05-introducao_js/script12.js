0f4e4d1fd79abab33e007cb8ab9c21d257404b93
BrunoSouza <desouza88@hotmail.com>
2022-09-21 21:03:07 -0300

    Resoluçaos dos exercicios de fixação JS

---
+++ fundamentos/-05-introducao_js/script12.js
@@ -0,0 +1,11 @@
+const a = 4;
+const b = 3;
+const c = (a-b);
+
+if (c > 0) {
+  console.log ("Positivo");
+} else if (c < 0) {
+    console.log ("Negativo");
+} else {
+    console.log ("Ninguém é de ninguém")
+}
