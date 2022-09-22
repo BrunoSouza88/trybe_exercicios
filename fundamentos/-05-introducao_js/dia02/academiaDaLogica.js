3952ba127c7c9765d92079ee2cf036398e978afe
BrunoSouza <desouza88@hotmail.com>
2022-09-22 13:22:23 -0300

    iniciando exercicios da academia da logica sobre JS

---
+++ fundamentos/-05-introducao_js/dia02/academiaDaLogica.js
@@ -0,0 +1,11 @@
+let idadeMarina = 25;
+let idadeSilvia = 30;
+let idadeIza = 45;
+
+if (idadeMarina < idadeSilvia && idadeMarina < idadeIza) {
+    console.log("Marina é a pessoa mais jovem, e possui " + idadeMarina + " anos de idade.")
+} else if (idadeSilvia < idadeMarina && idadeSilvia < idadeIza) {
+    console.log ("Silvi é a pessoa mais jovem, e possui " + idadeSilvia + " anos de idade.")
+} else {
+    console.log ("Iza é a pessoa mais jovem, e possui " + idadeIza + " anos de idade.")
+}
