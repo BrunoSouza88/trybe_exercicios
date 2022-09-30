22cf08c747d3bf356089665768e76ae7605560c9
BrunoSouza <desouza88@hotmail.com>
2022-09-29 21:17:21 -0300

    Realização da segunda parte dos exercicios

--- fundamentos/-06-dom_eventos_webStore/dia02/exercicio1_2.js
+++ fundamentos/-06-dom_eventos_webStore/dia02/exercicio1_2.js
@@ -45,4 +45,17 @@ for (index = 0; index < 3; index +=1) {
 }
 
 
+elementh1_1.className('title');
+
+let elementsH3 = document.getElementsByTagName('h3');
+
+for (let index = 0; index < 3; index += 1) {
+  elementsH3[index].className = 'description';
+}
+
+document.getElementsByClassName('left-content')[0].elementMain.removeChild(elementSection2);
+
+document.getElementsByClassName('right-content')[0].style.marginRight = 'auto';
+
+document.getElementsByClassName('main-content')[0].style.backgroundColor = 'green';
 
